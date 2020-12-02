6f9723d58c64f22f76d2ebb92adf1718381a1b72
zberkowitz <5496222+zberkowitz@users.noreply.github.com>
2020-12-02 15:07:44 -0600

    Update Express to serve static directory

--- UI/index.js
+++ UI/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
