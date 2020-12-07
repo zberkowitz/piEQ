0b16cbb8cd07a3cbcead98ee49aa27db224a1dc6
zberkowitz <5496222+zberkowitz@users.noreply.github.com>
2020-12-07 15:56:01 -0600

    Add OSC sending to Pd and basic graphic EQ patch

--- UI/index.js
+++ UI/index.js
@@ -1,9 +1,44 @@
+var osc = require('osc')
+
 const express = require('express')
 const app = express()
 const port = 80
 
+var bodyParser = require('body-parser')
+
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`piEQ listening at http://localhost:${port}`)
+})
+
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 3001,
+    metadata: true
+});
+
+udpPort.open();
+
+function sendToPD(filter, param, value){
+	var address = "/" + filter + "/" + param;
+	udpPort.send({
+		address: address,
+		args: [
+			{
+				type: "f",
+				value: value
+			}
+		]
+	}, "localhost", 3000);
+}
+
+app.post('/control', function (req, res){
+	//console.log("gotit");
+	console.log(req.body);
+	sendToPD(req.body.filter, req.body.param, req.body.value);
+	res.end();
 })
